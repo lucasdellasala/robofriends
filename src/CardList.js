37c56cd972313191533f7b8408133dd47a315b53
Lucas Della Sala <lucasdellasalamarkovic@gmail.com>
2020-03-01 19:21:49 -0300

    Refactorizando

--- src/CardList.js
+++ src/CardList.js
@@ -3,10 +3,15 @@ import Card from "./Card";
 
 const CardList = ({robots}) => {
     const cardArray = robots.map((user, i) => {
-        return <Card key={i} id={robots[i].id} name={robots[i].name} email={robots[i].email}/>
+        return (<Card 
+            key={i} 
+            id={robots[i].id} 
+            name={robots[i].name} 
+            email={robots[i].email}/>
+            );
     });
     return (
-        <div className="tc bg-light-blue dib br3 pa3 ma2 grow bw2 shadow-5">
+        <div>
             {cardArray}
         </div>
     );
