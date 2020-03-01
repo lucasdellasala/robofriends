fcf45a535c883cf961f56bc33dcf3172a2ed2336
Lucas Della Sala <lucasdellasalamarkovic@gmail.com>
2020-03-01 19:17:16 -0300

    Corrección nombre constante CardList

--- src/CardList.js
+++ src/CardList.js
@@ -2,12 +2,12 @@ import React from "react";
 import Card from "./Card";
 
 const CardList = ({robots}) => {
-    const cardComponent = robots.map((user, i) => {
+    const cardArray = robots.map((user, i) => {
         return <Card id={robots[i].id} name={robots[i].name} email={robots[i].email}/>
     });
     return (
         <div className="tc bg-light-blue dib br3 pa3 ma2 grow bw2 shadow-5">
-            {cardComponent}
+            {cardArray}
         </div>
     );
 }
