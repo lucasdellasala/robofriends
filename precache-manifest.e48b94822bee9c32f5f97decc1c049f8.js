0a148eb6c5874bfbce92d3944fdae47fdfad24c4
Lucas Della Sala <lucasdellasalamarkovic@gmail.com>
2020-04-30 05:11:27 -0300

    Updates

---
+++ precache-manifest.e48b94822bee9c32f5f97decc1c049f8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15810c3def27ea24813bb36b4ee582d4",
+    "url": "/lucasdellasala/robofriends/index.html"
+  },
+  {
+    "revision": "eb636cefb141c9e8213b",
+    "url": "/lucasdellasala/robofriends/static/css/2.c6861289.chunk.css"
+  },
+  {
+    "revision": "0349d59523103dc18792",
+    "url": "/lucasdellasala/robofriends/static/css/main.183e8768.chunk.css"
+  },
+  {
+    "revision": "eb636cefb141c9e8213b",
+    "url": "/lucasdellasala/robofriends/static/js/2.e2f0e0af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/lucasdellasala/robofriends/static/js/2.e2f0e0af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0349d59523103dc18792",
+    "url": "/lucasdellasala/robofriends/static/js/main.997e1340.chunk.js"
+  },
+  {
+    "revision": "8ea1a655bf347bc5a342",
+    "url": "/lucasdellasala/robofriends/static/js/runtime-main.33552415.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/lucasdellasala/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
