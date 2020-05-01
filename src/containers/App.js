0ecf7c4f2320b4933908b5c8dc1116a5e118e7eb
Lucas Della Sala <lucasdellasalamarkovic@gmail.com>
2020-04-30 21:20:47 -0300

    g

--- src/containers/App.js
+++ src/containers/App.js
@@ -24,21 +24,22 @@ class App extends Component {
             mode: "cors",
             cache: "default"
         };
-        fetch("https://thingproxy.freeboard.io/fetch/https://jsonplaceholder.typicode.com/users", miInit).then(function(response) {
-            if(response.ok) {
-              response.blob().then(function(miBlob) {
-                var objectURL = URL.createObjectURL(miBlob);
-                miLista.src = objectURL;
-              });
-            } else {
-              console.log('Respuesta de red OK pero respuesta HTTP no OK');
-            }
-          })
-          .catch(function(error) {
-            console.log('Hubo un problema con la petición Fetch:' + error.message);
-          });
-        //.then(response => response.json())
-        //.then(users => this.setState( { robots : users} ));
+        fetch("https://thingproxy.freeboard.io/fetch/https://jsonplaceholder.typicode.com/users", miInit)
+        // .then(function(response) {
+        //     if(response.ok) {
+        //       response.blob().then(function(miBlob) {
+        //         var objectURL = URL.createObjectURL(miBlob);
+        //         miLista.src = objectURL;
+        //       });
+        //     } else {
+        //       console.log('Respuesta de red OK pero respuesta HTTP no OK');
+        //     }
+        //   })
+        //   .catch(function(error) {
+        //     console.log('Hubo un problema con la petición Fetch:' + error.message);
+        //   });
+        .then(response => response.json())
+        .then(users => this.setState( { robots : users} ));
     }
 
     onSearchChange = (event) => {
